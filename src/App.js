1d7ec9db3fbf4973becfdfb2476bce4ea8239124
Zabdiel Rosas <zabdiel_rosas@outlook.com>
2022-11-13 23:44:16 -0600

    feat(App): context provider for user context is implemented

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
+import { useState } from 'react'
+// React Router DOM library
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   Link
 } from 'react-router-dom'
-
+// Context
+import { UserContext } from './context/UserContext'
 // Components
 import Index from './pages';
 import About from './pages/about';
 
 function App() {
+  // States
+  const [currentUser, setCurrentUser] = useState(null)
+
   return (
     <Router>
       <div>
@@ -24,10 +30,14 @@ function App() {
           </ul>
         </nav>
 
-        <Routes>
-          <Route index element={<Index/>} />
-          <Route path='about' element={<About />} />
-        </Routes>
+        <UserContext.Provider
+          value={{ currentUser, setCurrentUser }}
+        >
+          <Routes>
+            <Route index element={<Index/>} />
+            <Route path='about' element={<About />} />
+          </Routes>
+        </UserContext.Provider>
       </div>
     </Router>
   );
